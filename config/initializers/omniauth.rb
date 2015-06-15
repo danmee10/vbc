d296ff91645ad606a7d3b27c1d581ee372f4a477
Dan Mee <danmee10@gmail.com>
2015-06-14 18:41:46 -0600

    explicitely set requested user fields on omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :linkedin, ENV['LINKEDIN_KEY'], ENV['LINKEDIN_SECRET']
+  provider :linkedin, ENV['LINKEDIN_KEY'], ENV['LINKEDIN_SECRET'], fields: ['id', 'specialties', 'summary', 'email-address', 'first-name', 'last-name', 'headline', 'location', 'industry', 'picture-url', 'public-profile-url', 'positions']
 end
