608e530d17d154343ef71080acbfb9ea8fc821d9
Dan Mee <danmee10@gmail.com>
2015-06-14 18:41:04 -0600

    added user-cards associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ActiveRecord::Base
   has_many :authorizations, dependent: :destroy
+  has_many :cards, dependent: :destroy
 
   def self.create_from_hash!(hash)
     create(name: hash['info']['name'])
   end
+
 end
